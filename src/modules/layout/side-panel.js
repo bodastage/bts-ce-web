d2f1e71925db59e3d4c04f5b8ee70fd287b0639a
Emmanuel Robert Ssebaggala <emmanuel.ssebaggala@bodastage.com>
2018-07-21 03:47:13 +0300

    Add module list to side panel

--- src/modules/layout/side-panel.js
+++ src/modules/layout/side-panel.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { connect } from 'react-redux';
 
-export default class SidePanel extends React.Component{
+class SidePanel extends React.Component{
     constructor(props){
         super(props)
     }
     
+    onClick = (name) => (e) => { 
+        e.preventDefault();
+        
+        this.props.dispatch({
+            type: 'ADD_TAB',
+            tab: name
+        });
+    }
+    
     render(){
         return (
-        <div class="">
+        <div>
                 <span className="dropdown-item-text legend w-100">Radio Access Network</span>
                 <a className="dropdown-item" href="#/netmgt"> <FontAwesomeIcon icon="sitemap" /> Network Browser</a>
                 <a className="dropdown-item" href="#/netaudit" ><FontAwesomeIcon icon="wrench"/> Network Audit</a>
@@ -20,10 +30,12 @@ export default class SidePanel extends React.Component{
                 <a className="dropdown-item" href="#"> <FontAwesomeIcon icon="cogs"/>  Processes</a>
                 <a className="dropdown-item" href="#"><FontAwesomeIcon icon="cog"/> Settings</a>
                 <a className="dropdown-item" href="#"><FontAwesomeIcon icon="user"/> Profile</a>
-                <a className="dropdown-item" href="#"><FontAwesomeIcon icon="question-circle"/>  Help</a>
+                <a className="dropdown-item" href="#" onClick={this.onClick('Help')}><FontAwesomeIcon icon="question-circle"/>  Help</a>
         </div>
         );
         
     }
     
 }
+
+export default connect()(SidePanel);
